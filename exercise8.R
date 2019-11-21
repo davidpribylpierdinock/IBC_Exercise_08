66dd538948323568a98f33556b4ec3323ab86889
John Hlavin <jhlavin@nd.edu>
2019-11-21 00:27:41 -0500

    tweaked part 2

--- exercise8.R
+++ exercise8.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 
 #Game score plot
-game <- read.table("UWvMSU_1-22-13.txt",, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+game <- read.table("UWvMSU_1-22-13.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 scoreMSU <- 0
 scoreUW <- 0
 len = nrow(game)
@@ -14,28 +14,29 @@ for(i in 1:len){
   else if(game[i,2]=="UW"){
     scoreUW = scoreUW + game[i,3]
   }
-  gm.df[i, ] <- c(t, scoreUW, scoreMSU)
+  gm.df[i,] <- c(t, scoreUW, scoreMSU)
 }
 plot(gm.df$Time, gm.df$UW, type = "l", xlab = "Time", ylab = "Score", col = "red")
 lines(gm.df$Time, gm.df$MSU, col = "green")
 
-
 #GUESSING GAME 
-code = sample(100, 1)
-i = 0
-guess = -1
-while(i < 10 & guess!=code){
-  guess = readline(prompt = "guess a number between 1 and 100:")
-  if(guess > code){
-    print("lower")
-    i = i+1
-  }
-  else if(guess < code){
-    print("higher")
-    i = i+1
-  }
-  else if (guess==code){
-    print("You WIN!")
+guess = function(){
+  set = c(1:100) 
+  code = sample(set, 1)
+  i = 0
+  guess = -1
+  while(i < 10 & guess!=code){
+    guess = readline(prompt = "guess a number between 1 and 100: ")
+    if(guess > code){
+      print("lower")
+      i = i+1
+    }
+    else if(guess < code){
+      print("higher")
+      i = i+1
+    }
+    else if (guess==code){
+      print("You WIN!")
+    }
   }
 }
-
